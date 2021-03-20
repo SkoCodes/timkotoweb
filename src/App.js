7ea064e47de3da4d83969be1b26348a5c5e67e03
vlagarto.agora <vlagarto.agora@gmail.com>
2021-03-20 13:05:15 +0800

    Initial commit operator home page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import RegistrationSuccess from './components/registration/RegistrationSuccess';
 import Player from './components/player/Player';
-import OperatorAgents from './components/operator/Agents';
+import OperatorAgents from './components/operator/index';
 import ProtectedRoute from './components/common/ProtectedRoute';
+import Footer from './components/common/Footer'
 
 class App extends React.Component {
   render() {
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Route path="/registersuccess" exact component={RegistrationSuccess} />
           <ProtectedRoute path="/player" exact component={Player} roles={["Player"]} />
           <ProtectedRoute path="/operator" exact component={OperatorAgents} roles={["Operator"]} />
+          <Footer />
         </Switch>
       </Router>
     )
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { useHistory } from 'react-router-dom';
+
+function ElevationScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+      target: window ? window() : undefined,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+export default function Navbar(props){
+    const history = useHistory();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleRedirect = () =>{
+        handleClose()
+    }
+
+    const checkUserType = (type) =>{
+        if(type = "Operator"){
+            return(
+                <>
+                    <MenuItem onClick={handleRedirect()}>Contest Points</MenuItem>
+                    <MenuItem onClick={handleRedirect()}>Agent List</MenuItem>
+                </>
+            )
+        }
+    }
+
+    return(
+        <div>
+            <ElevationScroll {...props}>
+                <AppBar color="default">
+                <Toolbar>
+                        <IconButton onClick={handleClick} edge="start" color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            {()=>checkUserType(props.type)}
+                        </Menu>
+                        <Typography variant="h6" style={{textAlign: 'center', flexGrow: 1}}>{props.title}</Typography>
+                </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+        </div>
+    )
+}
